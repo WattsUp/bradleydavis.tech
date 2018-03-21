c411da1f9970d555c69abeb355375a09be0ca36e
Bradley Davis <bradley.l.davis@wsu.edu>
2018-03-21 00:02:22 -0700

    Added Scenery, Teleportator Animation, and Title Sign

--- web/interactive-resume/main.js
+++ web/interactive-resume/main.js
@@ -1,5 +1,4 @@
 $(function() {
-	setInterval(toggleSign, 1000);
 	setInterval(blink, 4000);
 	$(document).scroll(updateScene);
 	updateScene();
@@ -7,17 +6,23 @@ $(function() {
 	blink();
 });
 
+$(window).resize(updateScene);
+
 function getSceneX() {
 	return $(document).scrollTop();
 }
 
-var flashSign = true;
 var scenes = $(".scene");
 var backgrounds = $(".scene-background");
+var farBackgrounds = $(".scene-far-background");
+var onTheGrounds = $(".on-the-ground");
 var bradSprite = $("#brad");
 var bradContainer = $("#brad-container");
+var bradJumpContainer = $("#brad-jump-container");
+var groundHeight = $(".ground").height();
 var speedScene = 1 / 1;
 var speedBackground = 1 / 2;
+var speedFarBackground = 1 / 3;
 var timingWalking = 200;
 var timingBlink = 100;
 var teleporting = false;
@@ -30,40 +35,54 @@ var stopWalkingTimeoutID = null;
 var lastSceneX = getSceneX();
 var disableWalking = false;
 
-function toggleSign() {
-	flashSign = !flashSign;
-	$("#instructions").css("background-color",
-			flashSign ? "#282828" : "#33FF33");
-	$("#instructions").css("color", flashSign ? "#33FF33" : "#282828");
-}
 
 function teleporterChange(locationHash) {
 	if (!teleporting) {
 		teleporting = true;
 		bradContainer.css("left", "175px");
-		bradContainer.css("bottom", "235px");
 		bradContainer.css("transform", "translateX(-50%)");
-		bradContainer.addClass("jump-up-small");
-		bradContainer.removeClass("jump-down-small");
-		$("#teleporter-science").show();
+		bradContainer.addClass("ease-left");
+		bradJumpContainer.addClass("jump-up-small");
+		bradJumpContainer.removeClass("jump-down-small");
+		scenes.addClass("ease-transform");
+		backgrounds.addClass("ease-transform");
+		farBackgrounds.addClass("ease-transform");
+		scenes.css("transform", "translateX(0px)");
+		backgrounds.css("transform", "translateX(0px)");
+		farBackgrounds.css("transform", "translateX(0px)");
 		console.log(locationHash);
+		movingRight = false;
+		updateWalkingSprite();
+		setTimeout(updateWalkingSprite, timingWalking);
+		setTimeout(updateWalkingSprite, timingWalking * 2);
+		setTimeout(updateWalkingSprite, timingWalking * 3);
+		setTimeout(function() {
+			scenes.removeClass("ease-transform");
+			backgrounds.removeClass("ease-transform");
+			farBackgrounds.removeClass("ease-transform");
+			bradJumpContainer.css("bottom", "30px");
+		}, 1000);
+		setTimeout(function() {
+			$("#teleporter-science").show();
+		}, 1400);
 		setTimeout(function() {
 			$("#teleporter-science").hide();
 			bradContainer.hide();
 			teleporting = false;
-		}, 1000);
-		setTimeout(function() {
 			location.hash = locationHash;
+		}, 2000);
+		setTimeout(function() {
 			bradContainer.show();
 			teleporting = false;
 			bradContainer.css("left", "50%");
-			bradContainer.css("bottom", "200px");
-			bradContainer.addClass("jump-down-small");
-			bradContainer.removeClass("jump-up-small");
+			bradContainer.removeClass("ease-left");
+			bradJumpContainer.css("bottom", "0px");
+			bradJumpContainer.removeClass("jump-up-small");
 			updateScene();
-		}, 1500);
+		}, 2500);
 	}
 }
+
 
 function blink() {
 	if (movingRight) {
@@ -78,6 +97,7 @@ function blink() {
 		$("#brad-eyes-left").hide();
 	}, timingBlink);
 }
+
 
 function updateWalkingSprite() {
 	bradSprite.css("bottom", (movingRight * -200) + "px");
@@ -98,23 +118,26 @@ function updateWalkingSprite() {
 	}
 }
 
+
 function updateScene() {
 	var sceneX = getSceneX();
 	console.log("sceneX: " + sceneX);
 	updateMovement(sceneX);
 	updateStory(sceneX);
+	groundHeight = $(".ground").height();
+	onTheGrounds.css("bottom", groundHeight + "px");
 }
 
 function updateStory(sceneX) {
 	if (sceneX > 480) {
-		bradContainer.addClass("jump-up-small");
-		bradContainer.removeClass("jump-down-small");
-		bradContainer.css("bottom", "255px");
+		bradJumpContainer.addClass("jump-up-small");
+		bradJumpContainer.removeClass("jump-down-small");
+		bradJumpContainer.css("bottom", "55px");
 		disableWalking = true;
 	} else {
-		bradContainer.addClass("jump-down-small");
-		bradContainer.removeClass("jump-up-small");
-		bradContainer.css("bottom", "200px");
+		bradJumpContainer.addClass("jump-down-small");
+		bradJumpContainer.removeClass("jump-up-small");
+		bradJumpContainer.css("bottom", "0px");
 		disableWalking = false;
 	}
 	var roverPosition = Math.max(-500 + sceneX * speedScene, 0);
@@ -129,9 +152,13 @@ function updateMovement(sceneX) {
 	var positionBrad = -bradContainer.width() / 2 - Math.max(positionScene, 0);
 	// Backgrounds are moved forward by scene, move them backwards
 	var positionBackground = adjustedSceneX * (speedScene - speedBackground);
+	var positionFarBackground = adjustedSceneX
+			* (speedScene - speedFarBackground);
 	scenes.css("transform", "translateX(" + Math.min(positionScene, 0) + "px)");
 	backgrounds.css("transform", "translateX("
 			+ Math.max(positionBackground, 0) + "px)");
+	farBackgrounds.css("transform", "translateX("
+			+ Math.max(positionFarBackground, 0) + "px)");
 	bradContainer.css("transform", "translateX(" + Math.min(positionBrad, 0)
 			+ "px)");
 	if (sceneX < lastSceneX) {
