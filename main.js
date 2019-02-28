6639aa2194cbeb9ebe67d4f7ec959d2d962ad4ed
Bradley Davis <me@bradleydavis.tech>
2019-02-27 18:04:35 -0800

    Wrote Contact Me Page

--- main.js
+++ main.js
@@ -2,9 +2,22 @@
  * Function once page is fully loaded
  */
 $(window).bind('load', function() {
+  updateFoodMessage();
+});
+
+/**
+ * Changes the message about the current food day
+ * Reruns at midnight
+ */
+function updateFoodMessage() {
   var menu = [
     'Sundae Sunday', 'Meatloaf Monday', 'Thai Tuesday', 'Waffle Wednesday',
     'Thai Thursday', 'Fried Food Friday', 'Salmon Saturday'
   ];
   $('#food-message').text(menu[(new Date()).getDay()]);
-});
+  var today = new Date();
+  var tomorrow =
+      new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1);
+  var timeToMidnight = tomorrow - today;
+  setTimeout(updateFoodMessage, timeToMidnight);
+}
