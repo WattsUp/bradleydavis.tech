1bddac6b78891ecd2f6f07bb9ce43240e3562a25
Bradley Davis <me@bradleydavis.tech>
2019-04-15 16:02:00 -0700

    Added Culinary Page

--- masonry.js
+++ masonry.js
@@ -3,10 +3,10 @@
  */
 $(window).bind('load', function() {
   resizeMasonry();
+  $(window).bind('resize', resizeMasonry);
+  $(window).bind('orientationChange', resizeMasonry);
   setTimeout(resizeMasonry, 2000);
 });
-
-window.onresize = resizeMasonry;
 
 var roots = $('.masonry');
 var rootBricks = [];
@@ -26,17 +26,18 @@ function findBricks() {
  */
 function resizeMasonry() {
   findBricks();
-
-  for (var i = 0; i < roots.length; i++) {
-    var root = roots.eq(i);
-    var bricks = rootBricks[i];
-    var rowGap = parseInt(root.css('grid-row-gap'), 10);
-    var rowHeight = parseInt(root.css('grid-auto-rows'), 10);
-    for (var ii = 0; ii < bricks.length; ii++) {
-      var brick = bricks.eq(ii);
-      var height = brick.children().eq(0).height();
-      var rows = Math.ceil((height + rowGap) / (rowHeight + rowGap));
-      brick.css('grid-row-end', 'span ' + rows);
+  setTimeout(function() {
+    for (var i = 0; i < roots.length; i++) {
+      var root = roots.eq(i);
+      var bricks = rootBricks[i];
+      var rowGap = parseInt(root.css('grid-row-gap'), 10);
+      var rowHeight = parseInt(root.css('grid-auto-rows'), 10);
+      for (var ii = 0; ii < bricks.length; ii++) {
+        var brick = bricks.eq(ii);
+        var height = brick.children().eq(0).height();
+        var rows = Math.ceil((height + rowGap) / (rowHeight + rowGap));
+        brick.css('grid-row-end', 'span ' + rows);
+      }
     }
-  }
+  }, 10);
 }
