d248c5fdcaed1ecda166bc4824c08ff014a6061b
WattsUp <me@bradleydavis.tech>
2020-04-04 00:08:01 -0700

    Fixed footer animation

--- standardscript.js
+++ standardscript.js
@@ -1,126 +1,83 @@
+var footer = {
+  brad: null,
+  sprite: null,
+  interval: null,
+  footer: null,
+  lastPosition: 0,
+  moving: false,
+  movingRight: true,
+  frameTime: 200,
+  currentFrame: 0,
+  init: function() {
+    this.brad = document.getElementById('footer-brad');
+    this.sprite = this.brad.children[0];
+    this.footer = document.getElementsByTagName('footer')[0];
+
+    this.brad.addEventListener('click', this.moveManual, false);
+    this.moveManual();
+  },
+  moveManual: function() {
+    clearInterval(footer.interval);
+    footer.interval = setInterval(footer.move, 4000);
+    footer.move();
+  },
+  move: function() {
+    var width = footer.brad.offsetWidth;
+    var change = Math.floor((Math.random() * 1.5 + 0.5) * width);
+    var position = footer.brad.offsetLeft;
+    if (position - change < 0) {
+      position = position + change;
+    } else if (position + change > (footer.footer.offsetWidth - width)) {
+      position = position - change;
+    } else if (Math.random() > 0.5) {
+      position = position + change;
+    } else {
+      position = position - change;
+    }
+    footer.brad.style.left = position + 'px';
+    if (!footer.moving) {
+      footer.movingRight = footer.lastPosition < position;
+      footer.moving = true;
+      setTimeout(footer.walk, footer.frameTime);
+      setTimeout(footer.walk, footer.frameTime * 2);
+      setTimeout(footer.walk, footer.frameTime * 3);
+      setTimeout(footer.walk, footer.frameTime * 4);
+      setTimeout(footer.walk, footer.frameTime * 5);
+      setTimeout(footer.walk, footer.frameTime * 6);
+      setTimeout(footer.walk, footer.frameTime * 7);
+      setTimeout(footer.walk, footer.frameTime * 8);
+    }
+    footer.lastPosition = position;
+  },
+  walk: function() {
+    footer.sprite.style.bottom = (footer.movingRight * -7) + 'rem';
+    footer.currentFrame = (footer.currentFrame + 1) % 4;
+    switch (footer.currentFrame) {
+      case 0:
+      case 2:
+        footer.sprite.style.left = '0rem';
+        footer.moving = false;
+        break;
+      case 1:
+        footer.sprite.style.left = '-7rem';
+        break;
+      case 3:
+        footer.sprite.style.left = '-14rem';
+        break;
+    }
+  }
+};
+
 /**
  * Function once page is fully loaded
  */
-$(window).bind('load', function() {
-  requestAnimationFrame(animateMenuWheels);
-  $('#copyright-date').text((new Date()).getFullYear());
-  brad.click(moveFooterBradManual);
-  moveFooterBradManual();
-});
-
-/****************************** Footer Functions ******************************/
-var brad = $('.footer-brad');
-var bradSprite = $('.footer-brad-sprite');
-var footer = $('footer').eq(0);
-
-var bradInterval = 0;
-var currentWalkingFrame = 0;
-var lastBradPosition = 0;
-var moving = false;
-var movingRight = true;
-var timingWalking = 200;
-
-/**
- * Reset the interval timer and manually move the brad
- */
-function moveFooterBradManual() {
-  clearInterval(bradInterval);
-  bradInterval = setInterval(moveFooterBrad, 4000);
-  moveFooterBrad();
+function onLoad() {
+  document.getElementById('copyright-date').innerHTML =
+      (new Date()).getFullYear();
+  footer.init();
 }
 
-/**
- * Randomly move brad in the footer
- */
-function moveFooterBrad() {
-  var bradWidth = brad.width();
-  var range = footer.width() - bradWidth;
-  var change = Math.floor((Math.random() * 1.5 + 0.5) * bradWidth);
-  var position = brad.position().left;
-  if (position - change < 0) {
-    position = position + change;
-  } else if (position + change > range) {
-    position = position - change;
-  } else if (Math.random() > 0.5) {
-    position = position + change;
-  } else {
-    position = position - change;
-  }
-  brad.css('left', position + 'px');
-  // Animate sprite when walking
-  if (lastBradPosition > position) {
-    if (!moving) {
-      movingRight = false;
-      moving = true;
-      setTimeout(updateWalkingSprite, timingWalking);
-      setTimeout(updateWalkingSprite, timingWalking * 2);
-      setTimeout(updateWalkingSprite, timingWalking * 3);
-      setTimeout(updateWalkingSprite, timingWalking * 4);
-      setTimeout(updateWalkingSprite, timingWalking * 5);
-      setTimeout(updateWalkingSprite, timingWalking * 6);
-      setTimeout(updateWalkingSprite, timingWalking * 7);
-      setTimeout(updateWalkingSprite, timingWalking * 8);
-    }
-  } else {
-    if (!moving) {
-      movingRight = true;
-      moving = true;
-      setTimeout(updateWalkingSprite, timingWalking);
-      setTimeout(updateWalkingSprite, timingWalking * 2);
-      setTimeout(updateWalkingSprite, timingWalking * 3);
-      setTimeout(updateWalkingSprite, timingWalking * 4);
-      setTimeout(updateWalkingSprite, timingWalking * 5);
-      setTimeout(updateWalkingSprite, timingWalking * 6);
-      setTimeout(updateWalkingSprite, timingWalking * 7);
-      setTimeout(updateWalkingSprite, timingWalking * 8);
-    }
-  }
-  lastBradPosition = position;
-}
-
-/**
- * Change the sprite to the next walking frame
- */
-function updateWalkingSprite() {
-  bradSprite.css('bottom', (movingRight * -7) + 'rem');
-  currentWalkingFrame = (currentWalkingFrame + 1) % 4;
-  switch (currentWalkingFrame) {
-    case 0:
-    case 2:
-      bradSprite.css('left', '0rem');
-      moving = false;
-      break;
-    case 1:
-      bradSprite.css('left', '-7rem');
-      break;
-    case 3:
-      bradSprite.css('left', '-14rem');
-      break;
-  }
-}
-
-/******************************* Scroll Button ********************************/
-$(window).bind('scroll', scrollUpdate);
-$(window).bind('resize', scrollUpdate);
-$(window).bind('orientationChange', scrollUpdate);
-
-var buttonUp = $('.button-up');
-
-/**
- * Shows or hides the scrollUp button
- */
-function scrollUpdate() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    cssPrefix(buttonUp, 'transform', 'translateX(0rem)');
-  } else {
-    cssPrefix(buttonUp, 'transform', 'translateX(-5rem)');
-  }
-}
-
-/**
- * Scrolls to the top of the page
- */
-function buttonScrollUp() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+if (document.readyState == 'Loading')
+  window.addEventListener('DOMContentLoaded', onLoad);
+else
+  onLoad();
