e518d0e66a8ce622c084c2f2a96791b60b4fd1ac
Bradley Davis <me@bradleydavis.tech>
2020-04-16 14:06:56 -0700

    Added lab particle viewer

--- interactive-resume/index.js
+++ interactive-resume/index.js
@@ -2,10 +2,22 @@
  * Creation of dynamic scene elements, movement of the scene
  */
 var scene = {
+  labParticles: null,
+  labParticleMasks: [],
+  labParticleLightning: null,
+  labParticleCurrent: 0,
   /**
    * Initialize the scene, add listeners
    */
   init: function() {
+    scene.labParticles = document.getElementById('lab-particles')
+    scene.labParticleMasks =
+        document.querySelectorAll('#lab-particle-image>.mask');
+    scene.labParticleLightning =
+        document.getElementById('lab-particle-lightning')
+    setInterval(scene.changeLabParticles, 5000);
+    scene.changeLabParticles();
+
     window.addEventListener('scroll', scene.onFirstScroll);
   },
   /**
@@ -17,6 +29,24 @@ var scene = {
     setTimeout(function() {
       document.getElementById('scroll-down').hidden = true;
     }, 5000);
+  },
+  /**
+   * Change the current lab particle viewer mask
+   */
+  changeLabParticles: function() {
+    scene.labParticleLightning.hidden = false;
+    scene.labParticles.style.height = '0';
+
+    setTimeout(function() {
+      for (var i = 0; i < scene.labParticleMasks.length; i++)
+        scene.labParticleMasks[i].hidden = (i != scene.labParticleCurrent);
+      scene.labParticleCurrent =
+          (scene.labParticleCurrent + 1) % scene.labParticleMasks.length;
+      scene.labParticles.style.height = '100%';
+    }, 1500);
+    setTimeout(function() {
+      scene.labParticleLightning.hidden = true;
+    }, 2500);
   }
 };
 
