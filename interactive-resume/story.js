a368f76ca2ee7a119d841f92e2c90ca58b38d62b
Bradley Davis <me@bradleydavis.tech>
2020-04-16 10:00:05 -0700

    Improved consistent frame rate

--- interactive-resume/story.js
+++ interactive-resume/story.js
@@ -6,6 +6,7 @@ var story = {
   distance: [],
   distanceSpace: null,
   lastSceneX: 0,
+  scrollEventEnqueued: false,
   /**
    * Initialize the story, add listeners
    */
@@ -36,12 +37,12 @@ var story = {
    * @param {integer} y offset of close distance
    */
   setBackground: function(x, y) {
-    story.distance[0].style.left = x + 'px';
-    story.distance[1].style.left = (x / 1.5) + 'px';
-    story.distance[2].style.left = (x / 2) + 'px';
-    story.distance[3].style.left = (x / 4) + 'px';
-    story.distance[4].style.left = (x / 8) + 'px';
-    story.distanceSpace.style.left = (x / 40) + 'px';
+    story.distance[0].style.transform = 'translateX(' + x + 'px)';
+    story.distance[1].style.transform = 'translateX(' + (x / 1.5) + 'px)';
+    story.distance[2].style.transform = 'translateX(' + (x / 2) + 'px)';
+    story.distance[3].style.transform = 'translateX(' + (x / 4) + 'px)';
+    story.distance[4].style.transform = 'translateX(' + (x / 8) + 'px)';
+    story.distanceSpace.style.transform = 'translateX(' + (x / 40) + 'px)';
   }
 };
 
